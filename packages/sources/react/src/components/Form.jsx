3fdf729f8b92a48c83ed0aec5c9959b195673d4f
Jérémie van der Sande <jeremie@fleeters.io>
2019-05-20 09:45:38 +0200

    [Fix] Do not path onBlur/onFocus to the underlying form element

--- packages/sources/react/src/components/Form.jsx
+++ packages/sources/react/src/components/Form.jsx
@@ -26,15 +26,15 @@ export class Form extends React.Component {
   }
 
   render() {
-    const { onUpdate, onSubmit, ...props } = this.props
+    const { onUpdate, onSubmit, onBlur, onFocus, children, ...props } = this.props
     return (
       <Provider formallyHandler={this.handler}>
         <form
           {...props}
-          className={className(this.props.className, { invalid: !this.handler.isValid, valid: this.handler.isValid }, this.handler.formState)}
+          className={className(props.className, { invalid: !this.handler.isValid, valid: this.handler.isValid }, this.handler.formState)}
           onSubmit={this.onSubmit}
         >
-          {this.props.children}
+          {children}
         </form>
       </Provider>
     )
